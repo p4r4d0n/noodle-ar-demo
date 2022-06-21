434fa6bca7db59850fe95a76860e30d4963bf4ba
Paradon Petchmeesri <p.petchmeesri@gmail.com>
2022-06-21 17:31:33 +0700

    initial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative">
+    <iframe
+      src="/ar-scan.html"
+      frameborder="0"
+      width="100%"
+      class="w-full h-screen min-h-screen z-0"
+      scrolling="no"
+    ></iframe>
+    <a href="https://www.sahapat.co.th/product/foodproduct" class="z-50 fixed bottom-1 left-2">
+      <img src="/logo.png" class="w-10" alt="1123"/>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `plugins/**/*.{js,ts}`,
+    `nuxt.config.{js,ts}`,
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        db_heavent: ["DB_Heavent"],
+      },
+    },
+  },
+};
